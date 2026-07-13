e881769e9fdf2577db2619f9becae4961f8e2d0d
AditiGandhi2004 <121374286+AditiGandhi2004@users.noreply.github.com>
2026-07-13 22:15:11 +0530

    Update createAdmin.js

--- server/createAdmin.js
+++ server/createAdmin.js
@@ -1,38 +1,70 @@
 require("dotenv").config();
 
-const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const connectDB = require("./config/db");
 const Admin = require("./models/Admin");
 
+
 const createAdmin = async () => {
+
   try {
+
     await connectDB();
 
+
     const existing = await Admin.findOne({
-  username: "admin",
-});
-    if (existing) {
-      console.log("Admin already exists");
-      process.exit();
-    }
-
-    const password = await bcrypt.hash("admin123", 10);
-
-    await Admin.create({
-      name: "Hotel Admin",
-      email: "admin@asmati.com",
-      password,
+      username: "admin"
     });
 
+
+    if (existing) {
+
+      console.log("Admin already exists");
+      process.exit();
+
+    }
+
+
+    const hashedPassword = await bcrypt.hash(
+      "admin123",
+      10
+    );
+
+
+    await Admin.create({
+
+      name: "Hotel Admin",
+
+      username: "admin",
+
+      email: "admin@asmati.com",
+
+      password: hashedPassword
+
+    });
+
+
     console.log("Admin Created Successfully");
+
+    console.log("Username: admin");
+    console.log("Password: admin123");
+
+
     process.exit();
 
-  } catch (err) {
+
+  } catch(err) {
+
+
     console.log(err);
+
     process.exit(1);
+
+
   }
+
 };
+
 
 createAdmin();
