d12ce3a7a5160172481e8a46497ebea6b92097c4
AditiGandhi2004 <121374286+AditiGandhi2004@users.noreply.github.com>
2026-07-13 21:35:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Help from "./pages/Help";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
@@ -10,89 +11,72 @@ import Success from "./pages/Success";
 import Stays from "./pages/Stays";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
-import SignIn from "./pages/SignIn";
+
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminPayments from "./pages/AdminPayments";
 import AdminBookings from "./pages/AdminBookings";
-import Admin from "./pages/Admin";
+
+
 function App() {
   return (
     <BrowserRouter>
+
       <Routes>
 
-        <Route
-          path="/"
-          element={<Home />}
-        />
-<Route
-  path="/admin/payments"
-  element={<AdminPayments />}
-/>
-<Route 
+        {/* User Pages */}
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/signin" element={<Login />} />
+
+        <Route path="/booking" element={<Booking />} />
+
+        <Route path="/summary" element={<BookingSummary />} />
+
+        <Route path="/payment" element={<Payment />} />
+
+        <Route path="/success" element={<Success />} />
+
+        <Route path="/stays" element={<Stays />} />
+
+        <Route path="/gallery" element={<Gallery />} />
+
+        <Route path="/contact" element={<Contact />} />
+
+        <Route path="/help" element={<Help />} />
+
+
+        {/* Admin */}
+
+        <Route 
           path="/admin" 
-          element={<Admin />} 
-        />
-<Route
-  path="/admin/bookings"
-  element={<AdminBookings />}
-/>
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-<Route
-  path="/help"
-  element={<Help />}
-/>
-        <Route
-          path="/signin"
-          element={<Login />}
+          element={<AdminLogin />} 
         />
 
-        <Route
-          path="/booking"
-          element={<Booking />}
-        />
 
         <Route
-          path="/summary"
-          element={<BookingSummary />}
+          path="/admin/dashboard"
+          element={<AdminDashboard />}
         />
+
 
         <Route
-          path="/payment"
-          element={<Payment />}
+          path="/admin/bookings"
+          element={<AdminBookings />}
         />
+
 
         <Route
-          path="/success"
-          element={<Success />}
+          path="/admin/payments"
+          element={<AdminPayments />}
         />
-<Route
-  path="/stays"
-  element={<Stays />}
-/>
 
-<Route
-path="/gallery"
-element={<Gallery />}
-/>
-<Route path="/admin" element={<AdminLogin />} />
 
-<Route
-  path="/admin/dashboard"
-  element={<AdminDashboard />}
-/>
-<Route
-path="/contact"
-element={<Contact />}
-/>
-<Route
-  path="/signin"
-  element={<SignIn />}
-/>
       </Routes>
+
     </BrowserRouter>
   );
 }
